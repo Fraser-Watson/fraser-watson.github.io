8ef842320d7e0a2c0d83794e5e9e0cd339a519ae
Fraser-Watson <36534649+Fraser-Watson@users.noreply.github.com>
2019-02-26 12:29:37 +0000

    navbar bug fix

--- Projects/CryptoTracker Front End/js/burger.js
+++ Projects/CryptoTracker Front End/js/burger.js
@@ -1,7 +1,9 @@
 function openBurgerMenu(){
     document.getElementById('burger-menu').style.width="250px";
+    document.getElementsByClassName("container-body").style.marginRight = "250px";
 }
 
 function closeBurgerMenu(){
     document.getElementById('burger-menu').style.width="0px";
+    document.getElementsByClassName("container-body").style.marginRight = "0px";
 }
