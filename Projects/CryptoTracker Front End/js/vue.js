d36ef6fe9349c6f2faed4a3b3df4f53dc301b6a9
Fraser-Watson <36534649+Fraser-Watson@users.noreply.github.com>
2019-03-03 20:05:47 +0000

    Code refactor, colour codes

--- Projects/CryptoTracker Front End/js/vue.js
+++ Projects/CryptoTracker Front End/js/vue.js
@@ -1,229 +1,209 @@
-/* var app = new Vue({
+
+const CRYPTOCOMPARE_API_URI = "https://min-api.cryptocompare.com";
+
+const API_PARAMETERS = "/data/pricemultifull?fsyms=BTC,ETH,LTC,XRP,OMG,EOS,NEO,DASH,XMR&tsyms=USD"
+
+const UPDATE_INTERVAL = 5 * 1000;
+
+let app = new Vue({
   el: "#app",
+  mixins: [Vue2Filters.mixin],
   data: {
-    priceBtc: "",
-    hrHiBtc: "",
-    hrLowBtc: "",
-    volBtc: "",
-    mktCapBtc: ""
+    coins: {},
+    coinData: {}
   },
-
   methods: {
-    fetchBtc: function() {
-      axios
-        .get(
-          "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,LTC,XRP&tsyms=USD"
-        )
-        .then(function(response) {
-          this.priceBtc = response.data.RAW.BTC.USD.PRICE;
+
+    getCoins: function() {
+      let self = this;
+
+      axios.get(CRYPTOCOMPARE_API_URI + API_PARAMETERS)
+        .then((resp) => {
+          this.coins = resp.data;
         })
-        .catch(function(error) {
-          this.priceBtc = "Error";
+        .catch((err) => {
+          console.error(err);
         });
-    }
+    },
+
+    getColor: (num) => {
+      return num > 0 ? "color:#78C31D;" : "color:red;";
+    },
+
+    getColorUpdate: (updated) => {
+      return updated === "Just now" ? "color:#78C31D;" : "color: #ECB600;";
+    },
   },
 
-  created: function(){
-      this.fetchBtc()
+  created: function () {
+    this.getCoins();
   }
-})
+});
 
-*/
-
-
- var app = new Vue({
-    el: '#app',
-    data () {
-      return {
-        priceBtc: "",
-        hrHiBtc: "",
-        hrLowBtc: "",
-        volBtc: "",
-        mktCapBtc: "",
-        mktBtc: "",
-        splyBtc: "",
-        chgBtc: "",
-        updBtc: "",
-
-        priceEth: "",
-        hrHiEth: "",
-        hrLowEth: "",
-        volEth: "",
-        mktCapEth: "",
-        mktEth: "",
-        splyEth: "",
-        chgEth: "",
-        updEth: "",
-
-        priceLtc: "",
-        hrHiLtc: "",
-        hrLowLtc: "",
-        volLtc: "",
-        mktCapLtc: "",
-        mktLtc: "",
-        splyLtc: "",
-        chgLtc: "",
-        updLtc: "",
-
-        priceXrp: "",
-        hrHiXrp: "",
-        hrLowXrp: "",
-        volXrp: "",
-        mktCapXrp: "",
-        mktXrp: "",
-        splyXrp: "",
-        chgXrp: "",
-        updXrp: "",
-
-        priceOmg: "",
-        hrHiOmg: "",
-        hrLowOmg: "",
-        volOmg: "",
-        mktCapOmg: "",
-        mktOmg: "",
-        splyOmg: "",
-        chgOmg: "",
-        updOmg: "",
-
-        priceEos: "",
-        hrHiEos: "",
-        hrLowEos: "",
-        volEos: "",
-        mktCapEos: "",
-        mktEos: "",
-        splyEos: "",
-        chgEos: "",
-        updEos: "",
-
-        priceNeo: "",
-        hrHiNeo: "",
-        hrLowNeo: "",
-        volNeo: "",
-        mktCapNeo: "",
-        mktNeo: "",
-        splyNeo: "",
-        chgNeo: "",
-        updNeo: "",
-        
-        priceDash: "",
-        hrHiDash: "",
-        hrLowDash: "",
-        volDash: "",
-        mktCapDash: "",
-        mktDash: "",
-        splyDash: "",
-        chgDash: "",
-        updDash: "",
-
-        priceXmr: "",
-        hrHiXmr: "",
-        hrLowXmr: "",
-        volXmr: "",
-        mktCapXmr: "",
-        mktXmr: "",
-        splyXmr: "",
-        chgXmr: "",
-        updXmr: ""
-
-      }
-    },
-    mounted () {
-      axios
-        .get('https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,LTC,XRP,OMG,EOS,NEO,DASH,XMR&tsyms=USD&api_key=0bf19dd98d2e5dba83157ffb931d95419799e96b6edf0a1f30a6f934f3a72469')
-        .then(response => {
-            this.priceBtc = response.data.DISPLAY.BTC.USD.PRICE
-            this.hrHiBtc = response.data.DISPLAY.BTC.USD.HIGH24HOUR
-            this.hrLowBtc = response.data.DISPLAY.BTC.USD.LOW24HOUR
-            this.volBtc = response.data.DISPLAY.BTC.USD.VOLUME24HOUR
-            this.mktCapBtc = response.data.DISPLAY.BTC.USD.MKTCAP
-            this.mktBtc = response.data.DISPLAY.BTC.USD.MARKET
-            this.splyBtc = response.data.DISPLAY.BTC.USD.SUPPLY
-            this.chgBtc = response.data.DISPLAY.BTC.USD.CHANGE24HOUR
-            this.updBtc = response.data.DISPLAY.BTC.USD.LASTUPDATE
+setInterval(() => {
+  app.getCoins();
+}, UPDATE_INTERVAL);
 
 
-            this.priceEth = response.data.DISPLAY.ETH.USD.PRICE
-            this.hrHiEth = response.data.DISPLAY.ETH.USD.HIGH24HOUR
-            this.hrLowEth = response.data.DISPLAY.ETH.USD.LOW24HOUR
-            this.volEth = response.data.DISPLAY.ETH.USD.VOLUME24HOUR
-            this.mktCapEth = response.data.DISPLAY.ETH.USD.MKTCAP
-            this.mktEth = response.data.DISPLAY.ETH.USD.MARKET
-            this.splyEth = response.data.DISPLAY.ETH.USD.SUPPLY
-            this.chgEth = response.data.DISPLAY.ETH.USD.CHANGE24HOUR
-            this.updEth = response.data.DISPLAY.ETH.USD.LASTUPDATE
-
-            this.priceLtc = response.data.DISPLAY.LTC.USD.PRICE
-            this.hrHiLtc = response.data.DISPLAY.LTC.USD.HIGH24HOUR
-            this.hrLowLtc = response.data.DISPLAY.LTC.USD.LOW24HOUR
-            this.volLtc = response.data.DISPLAY.LTC.USD.VOLUME24HOUR
-            this.mktCapLtc = response.data.DISPLAY.LTC.USD.MKTCAP
-            this.mktLtc = response.data.DISPLAY.LTC.USD.MARKET
-            this.splyLtc = response.data.DISPLAY.LTC.USD.SUPPLY
-            this.chgLtc = response.data.DISPLAY.LTC.USD.CHANGE24HOUR
-            this.updLtc = response.data.DISPLAY.LTC.USD.LASTUPDATE
-
-            this.priceXrp = response.data.DISPLAY.XRP.USD.PRICE
-            this.hrHiXrp = response.data.DISPLAY.XRP.USD.HIGH24HOUR
-            this.hrLowXrp = response.data.DISPLAY.XRP.USD.LOW24HOUR
-            this.volXrp = response.data.DISPLAY.XRP.USD.VOLUME24HOUR
-            this.mktCapXrp = response.data.DISPLAY.XRP.USD.MKTCAP
-            this.mktXrp = response.data.DISPLAY.XRP.USD.MARKET
-            this.splyXrp = response.data.DISPLAY.XRP.USD.SUPPLY
-            this.chgXrp = response.data.DISPLAY.XRP.USD.CHANGE24HOUR
-            this.updXrp = response.data.DISPLAY.XRP.USD.LASTUPDATE
-
-            this.priceOmg = response.data.DISPLAY.OMG.USD.PRICE
-            this.hrHiOmg = response.data.DISPLAY.OMG.USD.HIGH24HOUR
-            this.hrLowOmg = response.data.DISPLAY.OMG.USD.LOW24HOUR
-            this.volOmg = response.data.DISPLAY.OMG.USD.VOLUME24HOUR
-            this.mktCapOmg = response.data.DISPLAY.OMG.USD.MKTCAP
-            this.mktOmg = response.data.DISPLAY.OMG.USD.MARKET
-            this.splyOmg = response.data.DISPLAY.OMG.USD.SUPPLY
-            this.chgOmg = response.data.DISPLAY.OMG.USD.CHANGE24HOUR
-            this.updOmg = response.data.DISPLAY.OMG.USD.LASTUPDATE
-
-            this.priceEos = response.data.DISPLAY.EOS.USD.PRICE
-            this.hrHiEos = response.data.DISPLAY.EOS.USD.HIGH24HOUR
-            this.hrLowEos = response.data.DISPLAY.EOS.USD.LOW24HOUR
-            this.volEos = response.data.DISPLAY.EOS.USD.VOLUME24HOUR
-            this.mktCapEos = response.data.DISPLAY.EOS.USD.MKTCAP
-            this.mktEos = response.data.DISPLAY.EOS.USD.MARKET
-            this.splyEos = response.data.DISPLAY.EOS.USD.SUPPLY
-            this.chgEos = response.data.DISPLAY.EOS.USD.CHANGE24HOUR
-            this.updEos = response.data.DISPLAY.EOS.USD.LASTUPDATE
-
-            this.priceNeo = response.data.DISPLAY.NEO.USD.PRICE
-            this.hrHiNeo = response.data.DISPLAY.NEO.USD.HIGH24HOUR
-            this.hrLowNeo = response.data.DISPLAY.NEO.USD.LOW24HOUR
-            this.volNeo = response.data.DISPLAY.NEO.USD.VOLUME24HOUR
-            this.mktCapNeo = response.data.DISPLAY.NEO.USD.MKTCAP
-            this.mktNeo = response.data.DISPLAY.NEO.USD.MARKET
-            this.splyNeo = response.data.DISPLAY.NEO.USD.SUPPLY
-            this.chgNeo = response.data.DISPLAY.NEO.USD.CHANGE24HOUR
-            this.updNeo = response.data.DISPLAY.NEO.USD.LASTUPDATE
-
-            this.priceDash = response.data.DISPLAY.DASH.USD.PRICE
-            this.hrHiDash = response.data.DISPLAY.DASH.USD.HIGH24HOUR
-            this.hrLowDash = response.data.DISPLAY.DASH.USD.LOW24HOUR
-            this.volDash = response.data.DISPLAY.DASH.USD.VOLUMEHOUR
-            this.mktCapDash = response.data.DISPLAY.DASH.USD.MKTCAP
-            this.mktDash = response.data.DISPLAY.DASH.USD.MARKET
-            this.splyDash = response.data.DISPLAY.DASH.USD.SUPPLY
-            this.chgDash = response.data.DISPLAY.DASH.USD.CHANGE24HOUR
-            this.updDash = response.data.DISPLAY.DASH.USD.LASTUPDATE
-
-            this.priceXmr = response.data.DISPLAY.XMR.USD.PRICE
-            this.hrHiXmr = response.data.DISPLAY.XMR.USD.HIGH24HOUR
-            this.hrLowXmr = response.data.DISPLAY.XMR.USD.LOW24HOUR
-            this.volXmr = response.data.DISPLAY.XMR.USD.VOLUMEHOUR
-            this.mktCapXmr = response.data.DISPLAY.XMR.USD.MKTCAP
-            this.mktXmr = response.data.DISPLAY.XMR.USD.MARKET
-            this.splyXmr = response.data.DISPLAY.XMR.USD.SUPPLY
-            this.chgXmr = response.data.DISPLAY.XMR.USD.CHANGE24HOUR
-            this.updXmr = response.data.DISPLAY.XMR.USD.LASTUPDATE
-        })
-        
+var popupBtc = document.getElementById("popupBtc");
+var btnBtc = document.getElementById("btnBtc");
+var closeBtnBtc = document.getElementById("closeBtnBtc");
+btnBtc.addEventListener('click', openPopupBtc);
+closeBtnBtc.addEventListener('click', closePopupBtc);
+window.addEventListener('click', clickOutsideBtc);
+function openPopupBtc() {
+    popupBtc.style.display = 'flex';
+}
+function closePopupBtc() {
+    popupBtc.style.display = 'none';
+}
+function clickOutsideBtc(e) {
+    if (e.target == popupBtc) {
+        popupBtc.style.display = 'none';
     }
-  })
+}
 
 
+var popupEth = document.getElementById("popupEth");
+var btnEth = document.getElementById("btnEth");
+var closeBtnEth = document.getElementById("closeBtnEth");
+btnEth.addEventListener('click', openPopupEth);
+closeBtnEth.addEventListener('click', closePopupEth);
+window.addEventListener('click', clickOutsideEth);
+function openPopupEth() {
+    popupEth.style.display = 'flex';
+}
+function closePopupEth() {
+    popupEth.style.display = 'none';
+}
+function clickOutsideEth(e) {
+    if (e.target == popupEth) {
+        popupEth.style.display = 'none';
+    }
+}
+
+var popupLtc = document.getElementById("popupLtc");
+var btnLtc = document.getElementById("btnLtc");
+var closeBtnLtc = document.getElementById("closeBtnLtc");
+btnLtc.addEventListener('click', openPopupLtc);
+closeBtnLtc.addEventListener('click', closePopupLtc);
+window.addEventListener('click', clickOutsideLtc);
+function openPopupLtc() {
+    popupLtc.style.display = 'flex';
+}
+function closePopupLtc() {
+    popupLtc.style.display = 'none';
+}
+function clickOutsideLtc(e) {
+    if (e.target == popupLtc) {
+        popupLtc.style.display = 'none';
+    }
+}
+
+var popupXrp = document.getElementById("popupXrp");
+var btnXrp = document.getElementById("btnXrp");
+var closeBtnXrp = document.getElementById("closeBtnXrp");
+btnXrp.addEventListener('click', openPopupXrp);
+closeBtnXrp.addEventListener('click', closePopupXrp);
+window.addEventListener('click', clickOutsideXrp);
+function openPopupXrp() {
+    popupXrp.style.display = 'flex';
+}
+function closePopupXrp() {
+    popupXrp.style.display = 'none';
+}
+function clickOutsideXrp(e) {
+    if (e.target == popupXrp) {
+        popupXrp.style.display = 'none';
+    }
+}
+
+var popupOmg = document.getElementById("popupOmg");
+var btnOmg = document.getElementById("btnOmg");
+var closeBtnOmg = document.getElementById("closeBtnOmg");
+btnOmg.addEventListener('click', openPopupOmg);
+closeBtnOmg.addEventListener('click', closePopupOmg);
+window.addEventListener('click', clickOutsideOmg);
+function openPopupOmg() {
+    popupOmg.style.display = 'flex';
+}
+function closePopupOmg() {
+    popupOmg.style.display = 'none';
+}
+function clickOutsideOmg(e) {
+    if (e.target == popupOmg) {
+        popupOmg.style.display = 'none';
+    }
+}
+
+var popupEos = document.getElementById("popupEos");
+var btnEos = document.getElementById("btnEos");
+var closeBtnEos = document.getElementById("closeBtnEos");
+btnEos.addEventListener('click', openPopupEos);
+closeBtnEos.addEventListener('click', closePopupEos);
+window.addEventListener('click', clickOutsideEos);
+function openPopupEos() {
+    popupEos.style.display = 'flex';
+}
+function closePopupEos() {
+    popupEos.style.display = 'none';
+}
+function clickOutsideEos(e) {
+    if (e.target == popupEos) {
+        popupEos.style.display = 'none';
+    }
+}
+
+var popupNeo = document.getElementById("popupNeo");
+var btnNeo = document.getElementById("btnNeo");
+var closeBtnNeo = document.getElementById("closeBtnNeo");
+btnNeo.addEventListener('click', openPopupNeo);
+closeBtnNeo.addEventListener('click', closePopupNeo);
+window.addEventListener('click', clickOutsideNeo);
+function openPopupNeo() {
+    popupNeo.style.display = 'flex';
+}
+function closePopupNeo() {
+    popupNeo.style.display = 'none';
+}
+function clickOutsideNeo(e) {
+    if (e.target == popupNeo) {
+        popupNeo.style.display = 'none';
+    }
+}
+
+var popupDash = document.getElementById("popupDash");
+var btnDash = document.getElementById("btnDash");
+var closeBtnDash = document.getElementById("closeBtnDash");
+btnDash.addEventListener('click', openPopupDash);
+closeBtnDash.addEventListener('click', closePopupDash);
+window.addEventListener('click', clickOutsideDash);
+function openPopupDash() {
+    popupDash.style.display = 'flex';
+}
+function closePopupDash() {
+    popupDash.style.display = 'none';
+}
+function clickOutsideDash(e) {
+    if (e.target == popupDash) {
+        popupDash.style.display = 'none';
+    }
+}
+
+var popupXmr = document.getElementById("popupXmr");
+var btnXmr = document.getElementById("btnXmr");
+var closeBtnXmr = document.getElementById("closeBtnXmr");
+btnXmr.addEventListener('click', openPopupXmr);
+closeBtnXmr.addEventListener('click', closePopupXmr);
+window.addEventListener('click', clickOutsideXmr);
+function openPopupXmr() {
+    popupXmr.style.display = 'flex';
+}
+function closePopupXmr() {
+    popupXmr.style.display = 'none';
+}
+function clickOutsideXmr(e) {
+    if (e.target == popupXmr) {
+        popupXmr.style.display = 'none';
+    }
+}
