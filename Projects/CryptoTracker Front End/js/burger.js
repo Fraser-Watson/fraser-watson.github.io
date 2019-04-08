d7052e413837f8b9d5e3356d9750cd5d02a42acc
Fraser-Watson <36534649+Fraser-Watson@users.noreply.github.com>
2019-04-08 13:50:14 +0100

    folder rename

--- Projects/CryptoTracker Front End/js/burger.js
+++
@@ -1,7 +0,0 @@
-function openBurgerMenu(){
-    document.getElementById('burger-menu').style.width="250px";
-}
-
-function closeBurgerMenu(){
-    document.getElementById('burger-menu').style.width="0px";
-}
---
+++ Projects/CryptoTracker/js/burger.js
@@ -0,0 +1,7 @@
+function openBurgerMenu(){
+    document.getElementById('burger-menu').style.width="250px";
+}
+
+function closeBurgerMenu(){
+    document.getElementById('burger-menu').style.width="0px";
+}
